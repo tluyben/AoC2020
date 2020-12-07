a950f141b7b6c5d752eabed85dd4b16f7f1d8b7a
Tycho Luyben <tycho@e-lab.nl>
2020-12-07 12:20:10 +0100

    removed comments

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,17 +11,6 @@ buf.lines()
     .map(|l| l.unwrap().parse::<i32>().unwrap())
     .collect()
 }
-/*
-fn lines_from_file2(filename: impl AsRef<Path>) -> Vec<i32> {
-    let mut result: Vec<i32> = Vec::new();
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    for line in buf.lines() {
-        result.push(line.unwrap().parse::<i32>().unwrap());
-    }
-    return result;
-}
-    */
 
 fn main() {
 
