4ad22949cf10c8453cd5a84ca85a5698514326c2
Tycho Luyben <tycho@e-lab.nl>
2020-12-14 14:49:41 +0100

    racket day6

---
+++ day6/day6.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+(define (kwic-read filename)
+  (with-input-from-file filename
+    (λ ()
+      (for/list ([line (in-lines)])
+        line))))
+
+(define input (kwic-read "1.txt"))
+
+(define cs (append (map (λ (y) (string->list y)) input) (list '())))
+
+(define (my-flatten lst)
+  (cond ((null? lst) '())
+        ((pair? lst)
+         (append (my-flatten (car lst)) (my-flatten (cdr lst))))
+        (else (list lst))))
+
+(define count 0)
+(define mem (make-hash))
+
+(define (lm char) (map (λ (x)
+     (cond ((= (hash-ref mem x 0) 1) 0)
+           (else (hash-set! mem x 1) 1))) char))
+(define mask (map (λ (l) (if (null? l) 0 1)) cs))
+(define clist (map (λ (l m)
+                     (if (= m 0) (hash-clear! mem) 0)
+                     (lm l)
+                     )
+                   cs mask))
+(define (lm2 char) (map (λ (x)
+     (hash-set! mem x (+ (hash-ref mem x 0) 1)) 1) char))
+
+(define (map-questions m c)
+  (map (λ (x)
+         (if (= x c) 1 0))
+         (my-flatten (hash-values m))))
+
+(define (all-questions m c)
+  (apply + (map-questions m c)))
+  
+(define tot 0)
+(define count2 0)
+(define clist2 (map (λ (l m)
+
+
+                     (if (= m 0) (set! tot (all-questions mem count)) 0)
+
+                     (if (> m 0) (list (set! count (+ count 1)) (lm2 l)) 0)
+                     (if (= m 0) (list (set! count 0) (hash-clear! mem)) 0)
+
+                     (if (= m 0) tot 0)
+                     
+                     )
+                   cs mask))
+
+(apply + (my-flatten clist))
+(apply + (my-flatten clist2))
+
